9356545fb8ce6668f9d3a0c31bb5c92162547331
learntopilk <jonsterivilja@gmail.com>
2018-08-05 13:39:21 +0300

    more variable work

--- utils/config.js
+++ utils/config.js
@@ -9,8 +9,10 @@ let port = process.env.PORT
 let mongoUrl = process.env.MONGODB_URI
 
 if (process.env.NODE_ENV === 'test') {
+  console.log('TEST ENVIRONMENT')
   port = process.env.TEST_PORT
   mongoUrl = process.env.TEST_MONGODB_URI
+  console.log('Process.env: ', process.env)
 }
 
 console.log(port, mongoUrl, "PORT AND MONGOURL")
